c094d53dbd0acf58e8e87b2a4a12c3ef9870dee8
Quratulain3 <78855356+Quratulain3@users.noreply.github.com>
2021-08-16 21:14:12 +0500

    Add files via upload

---
+++ littlekid.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  3 18:47:12 2021
+
+@author: Annie
+"""
+
+
+name= 'ann'
+age=13
+
+if name=='alice':
+    print('hi,alice')
+elif age<12:
+    print('you arent alice,Kiddo')
+else:
+   print ('you are neither alice,nor little kid')
